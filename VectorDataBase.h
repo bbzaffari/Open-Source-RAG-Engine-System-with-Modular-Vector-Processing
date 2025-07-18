af024214367aeb579f8041975f154c151894f4e3
BavZaffar <bruno.zaffari@edu.pucrs.br>
2025-07-18 11:56:28 -0300

    Update VectorDataBase.h

--- VectorDataBase.h
+++ VectorDataBase.h
@@ -26,7 +26,7 @@
 
 namespace VectorDataBase {
 
-    struct vdb_data { // Initialy on  #include "Chunk/ChunkCommons/ChunkCommons.h"
+    struct vdb_data { // vdb_data  was Initialy included on "Chunk/ChunkCommons/ChunkCommons.h"
         std::vector<float> flatVD;
         std::string vendor;
         std::string model;
